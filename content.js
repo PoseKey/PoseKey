7a2305733dcde64c5484d62e10d82f4fce9f6384
대엽 여 <21300443@handong.edu>
2018-09-09 17:09:32 +0900

    주석 추가

--- content.js
+++ content.js
@@ -1,8 +1,13 @@
 import * as tf from '@tensorflow/tfjs';
 import * as posenet from '@tensorflow-models/posenet';
 import Stats from 'stats.js';
-
+//global variables
 const stats = new Stats();
+
+
+/*
+ * 프로그램이 실행되면 실행되는 코드
+ */
 async function setup(){
     const video = await loadVideo();
     // console.log(video);
@@ -11,6 +16,10 @@ async function setup(){
     setupFPS();
     animate(video, model);
 }
+
+/**
+ * 카메라가 활성화된 HTMLVideoElement를 생성후 리턴
+ */
 async function loadVideo(){
     const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     let video = document.createElement('video');
@@ -21,6 +30,11 @@ async function loadVideo(){
     return video;
 }
 
+/**
+ * 매초 마다 pose를 detect합니다.
+ * @param {*} video // 카메라로 찍는 화면이 활성화된 HTMLVideoElement
+ * @param {*} model // posenet.PoseNet 모델
+ */
 function animate(video, model){
     async function detect(){
         stats.begin();
@@ -31,6 +45,9 @@ function animate(video, model){
     }
     detect();
 }
+/**
+ * 카메라로 1초에 몇번 인식하고 있는지 왼쪽 상단에 표시
+ */
 function setupFPS() {
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     document.body.appendChild(stats.dom);
