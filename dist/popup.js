fd660d8e2a50e2c6283da7494fe0b15484f286fe
Taeyeob <21300443@handong.edu>
2019-01-16 17:12:52 +0900

    git

---
+++ dist/popup.js
@@ -0,0 +1,99 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+      bobby: {
+        name: "Bobby Boone",
+        age: 25
+      },
+      john: {
+        name: "John Boby",
+        age: 35,
+      }
+    },
+    template: `
+      <div>
+        <h1>hello</h1>
+      </div>
+    `
+  })
+
+//   // import * as posenet from '@tensorflow-models/posenet';
+// // import { createSecureContext } from 'tls';
+
+// // navigator.getUserMedia = navigator.getUserMedia ||
+// //     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+// // const height = 480;
+// // const width = 640;
+// // let video;
+
+// setup();
+
+// chrome.runtime.onMessage.addListener(gotMessage);
+
+// function gotMessage(message, sender, sendResponse){
+//     console.log(message);
+// }
+
+// async function setup(){
+//     const button = document.getElementById('ON');
+//     button.onclick = ()=>{
+//         console.log("clicked");
+//         chrome.runtime.sendMessage(
+//             {data:"trigger"},
+//             (response)=>{
+//                 console.log(response);
+//             }
+//         );
+//     };
+//     // const button2 = document.getElementById('options');
+//     // button2.onclick = ()=>{
+//     //     chrome.runtime.openOptionsPage();
+//     // }
+// }
+// // const net = await posenet.load(0.75);
+
+//     // document.getElementById('loading').style.display = 'none';
+//     // document.getElementById('main').style.display = 'block';
+    
+//     // const show = await loadButton();
+//     // document.body.appendChild(show);
+//     // try {
+//     //     video = await loadVideo();
+//     // } catch (e) {
+//     //     let info = document.getElementById('info');
+//     //     info.textContent = 'this browser does not support video capture,' +
+//     //         'or this device does not have a camera\n';
+//     //     info.style.display = 'block';
+
+//     //     const button = document.createElement('button');
+//     //     button.innerText = "allow camera";
+//     //     button.onclick = function(){
+//     //         console.log("ACS");
+//     //     };
+
+//     //     info.appendChild(button);
+//     // }
+// // async function loadVideo(){
+// //     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+// //         throw new Error(
+// //             'Browser API navigator.mediaDevices.getUserMedia not available');
+// //     }
+// //     stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+// //     let video = document.getElementById('video');
+// //     video.height = 480;
+// //     video.width = 640;
+// //     video.srcObject = stream;
+// //     video.play();
+// //     return video;
+// // }
+
+// // async function loadButton(){
+// //     const show = document.createElement('button');
+// //     show.innerText = "show";
+// //     show.onclick = function(){
+// //         console.log("show");
+// //         chrome.tabs.sendMessage({}, {data: "require"});
+// //     };
+// //     return show;
+// // }
--- options.js
+++ options.js
@@ -2,7 +2,7 @@ import * as posenet from '@tensorflow-models/posenet';
 
 const videoWidth = 600;
 const videoHeight = 500;
-const stats = new Stats();
+// const stats = new Stats();
 
 navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
@@ -11,75 +11,20 @@ bindPage();
 
 export async function bindPage() {
     // Load the PoseNet model weights with architecture 0.75
-    const net = await posenet.load(0.75);
+    // const net = await posenet.load(0.75);
   
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('main').style.display = 'block';
+    // document.getElementById('loading').style.display = 'none';
+    // document.getElementById('main').style.display = 'block';
   
     let video;
-  
-    try {
-      video = await loadVideo();
-    } catch (e) {
-      let info = document.getElementById('info');
-      info.textContent = 'this browser does not support video capture,' +
-          'or this device does not have a camera';
-      info.style.display = 'block';
-      throw e;
-    }
-  
-    // setupGui([], net);
-    // setupFPS();
-    // detectPoseInRealTime(video, net);
+    video = await loadVideo();
   }
-
-function isAndroid() {
-  return /Android/i.test(navigator.userAgent);
-}
-
-function isiOS() {
-  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
-}
-
-function isMobile() {
-  return isAndroid() || isiOS();
-}
-
-/**
- * Loads a the camera to be used in the demo
- *
- */
-async function setupCamera() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    throw new Error(
-        'Browser API navigator.mediaDevices.getUserMedia not available');
-  }
-
-  const video = document.getElementById('video');
-  video.width = videoWidth;
-  video.height = videoHeight;
-
-  const mobile = isMobile();
-  const stream = await navigator.mediaDevices.getUserMedia({
-    'audio': false,
-    'video': {
-      facingMode: 'user',
-      width: mobile ? undefined : videoWidth,
-      height: mobile ? undefined : videoHeight,
-    },
-  });
-  video.srcObject = stream;
-
-  return new Promise((resolve) => {
-    video.onloadedmetadata = () => {
-      resolve(video);
-    };
-  });
-}
-
-async function loadVideo() {
-  const video = await setupCamera();
-  video.play();
-
-  return video;
+  async function loadVideo(){
+    stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    let video = document.getElementById('video');
+    video.height = 480;
+    video.width = 640;
+    video.srcObject = stream;
+    video.play();
+    return video;
 }
